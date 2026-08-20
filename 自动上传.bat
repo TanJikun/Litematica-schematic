4bf9d1de626e8714297deb88b6b6378dec43a9aa
TanJikun <wenhaoji007@163.com>
2026-08-20 21:04:08 +0800

    -

--- 自动上传.bat
+++ 自动上传.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d "D:\MC\.minecraft\versions\1.21.11-Fabric\schematics"   REM 改成你的仓库本地路径
+cd /d "%~dp0"
 git add .
 git commit -m "-"
 git push -u https://github.com/TanJikun/Litematica-schematic.git main --force
